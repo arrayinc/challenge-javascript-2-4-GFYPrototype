7375a03f1e64ef4f6b7e0e837844012678867292
jwelchman <j.welchman@yahoo.com>
2019-11-20 16:47:56 -0700

    finished challenge 4. good taste in books btw

--- answer.js
+++ answer.js
@@ -1,1 +1,10 @@
 // *** YOUR ANSWER BELOW ***
+
+let quotation = "\""
+let input = "Ray Bradbury is one of my favorite authors!"
+
+function quote(input) {
+    return quotation + input + quotation
+}
+
+console.log(quote("Fahrenheit " + 451));
